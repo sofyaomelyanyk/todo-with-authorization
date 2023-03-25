0200a705c71b0e3270fdc9ee55cc33aa21e40699
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2023-03-25 17:44:20 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./firebase";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/todo-with-authorization/">
       <Provider store={store}>
         <App />
       </Provider>
