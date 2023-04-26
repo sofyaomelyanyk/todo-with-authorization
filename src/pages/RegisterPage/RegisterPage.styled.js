07d6c62ead1253a3ded55502509cce5e904c3ec9
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2023-04-26 16:15:14 +0300

    add img

--- src/pages/RegisterPage/RegisterPage.styled.js
+++ src/pages/RegisterPage/RegisterPage.styled.js
@@ -10,7 +10,8 @@ export const Container = styled.div`
   width: 500px;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 40px;
+  margin-top: 15%;
+  background-color: rgba(255, 255, 255, 0.5);
   border: 2px solid rgb(209, 206, 206);
   border-radius: 15px;
   display: flex;
