74ed8d2d5a205fa02f2f034858d03fc4a54208c2
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2023-04-27 15:02:40 +0300

    change form validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,15 +4,27 @@ import s from "../Form/Form.module.css";
 export const Form = ({ title, handleClick }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("Email is required");
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Неккоректный Email");
+    } else setEmailError("");
+  };
 
   return (
     <div className={s.form}>
       <div className={s["form-field"]}>
+        
         <label className={s.text}>Email</label>
+        {emailError && <div>{emailError}</div>}
         <input
           type="email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => emailHandler(e)}
           placeholder="Email"
         />
       </div>
