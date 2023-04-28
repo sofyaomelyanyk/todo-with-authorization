9517679374216ab16a40ab81784b45d1151c70bf
Sofya Omelyanyk <sofyaomelyanyk@gmail.com>
2023-04-28 16:20:15 +0300

    add changes

--- src/pages/RegisterPage/RegisterPage.styled.js
+++ src/pages/RegisterPage/RegisterPage.styled.js
@@ -52,7 +52,7 @@ export const Wrap = styled.div`
 export const Container = styled.div`
   width: 100%;
 
-  padding: 60px 30px;
+  padding: 60px 20px;
   background-color: rgb(255, 255, 255);
   -webkit-box-shadow: 10px 3px 18px 3px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: 10px 3px 18px 3px rgba(0, 0, 0, 0.75);
